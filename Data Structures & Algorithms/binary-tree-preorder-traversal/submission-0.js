5cc9e3c84bf967ec437fe57199c340044a7aa6a1
Piotr Zieliński <piotr.zielinski.dev@gmail.com>
2026-04-03 14:32:24 +0200

    Add: binary-tree-preorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-0.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    preorderTraversal(root) {
+        const traversalArr = []
+        const traversalHelper = (node) =>{
+            if(!node) return
+            traversalArr.push(node.val)
+            if(!!node.left){
+                traversalHelper(node.left)
+            }
+            if(!!node.right){
+                traversalHelper(node.right)
+            }
+            return
+        }
+        traversalHelper(root)
+        return traversalArr
+    }
+}
